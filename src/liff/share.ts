7a29e0165a0ae629f98ed93005013206b7ad8ece
NiJia <louis70109@gmail.com>
2020-09-01 22:18:00 +0800

    chore: move liff/share to src/

---
+++ src/liff/share.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express/index";
+import { flexUrlTemplate } from "../../types/flexTemplate";
+import { FlexMessage } from "@line/bot-sdk";
+import { buildFlexContent, generateFlex } from "../../utils/flex";
+
+export function shareController(req: Request, res: Response) {
+  const query: any = req.query;
+  const flexQuery: flexUrlTemplate = {
+    title: query.title,
+    place: query.place,
+    time: query.time,
+    url: query.url,
+    description: query.desc,
+  };
+  const flex: FlexMessage = buildFlexContent(
+    query.title,
+    generateFlex(flexQuery, true)
+  );
+
+  res.render("share", {
+    liffId: process.env.CONCAT_ID,
+    flex: JSON.stringify(flex),
+  });
+}
