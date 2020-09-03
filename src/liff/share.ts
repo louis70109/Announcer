ad5b9d02534c7db3621f5c748437ebee5e64aa22
NiJia <louis70109@gmail.com>
2020-09-03 17:21:49 +0800

    chore: use template query string

--- src/liff/share.ts
+++ src/liff/share.ts
@@ -1,25 +1,39 @@
 import { Request, Response } from "express/index";
-import { flexUrlTemplate } from "../../types/flexTemplate";
+import { flexUrlTemplate, staffList } from "../../types/flexTemplate";
 import { FlexMessage } from "@line/bot-sdk";
-import { buildFlexContent, generateFlex } from "../../utils/flex";
+import {
+  buildFlexContent,
+  generateFlex,
+  activitySchedule,
+} from "../../utils/flex";
 
 export function shareController(req: Request, res: Response) {
   const query: any = req.query;
-  const flexQuery: flexUrlTemplate = {
-    title: query.title,
-    place: query.place,
-    time: query.time,
-    url: query.url,
-    description: query.desc,
-    activity: query.activity,
-  };
+  let flex: any = {};
+  if (query.template === 1) {
+    const flexQuery: flexUrlTemplate = {
+      title: query.title,
+      place: query.place,
+      time: query.time,
+      url: query.url,
+      description: query.desc,
+      activity: query.activity,
+    };
+    flex = buildFlexContent(query.title, generateFlex(flexQuery, true));
+  } else {
+    const staffQuery: staffList = {
+      title: query.title,
+      place: query.place,
+      url: query.url,
+      activity: query.activity,
+    };
+
+    flex = buildFlexContent(query.title, activitySchedule(staffQuery));
+  }
   // use query.template to judge different template
-
-  const flex: FlexMessage = buildFlexContent(
-    query.title,
-    generateFlex(flexQuery, true)
-  );
-
+  if (!flex) {
+    flex = { type: "text", text: "Message" };
+  }
   res.render("share", {
     liffId: process.env.CONCAT_ID,
     flex: JSON.stringify(flex),
