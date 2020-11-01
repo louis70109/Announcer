8b83d279d26a6175ba21127c67dd56dd7cbeb404
NiJia <louis70109@gmail.com>
2020-11-01 22:23:46 +0800

    chore: format

--- src/index.ts
+++ src/index.ts
@@ -3,54 +3,61 @@ import {
   buildFlexContent,
   activitySchedule,
   personalCard,
-} from "../utils/flex";
-import { Client } from "@line/bot-sdk";
-import { FlexMessage } from "@line/bot-sdk/lib/types";
-import { Card, flexUrlTemplate, staffList } from "../types/flexTemplate";
-import { buildCarouselContent } from "../utils/common";
+} from '../utils/flex';
+import { Client } from '@line/bot-sdk';
+import { FlexMessage } from '@line/bot-sdk/lib/types';
+import { Card, flexUrlTemplate, staffList } from '../types/flexTemplate';
+import { buildCarouselContent } from '../utils/common';
 
 // create LINE SDK config from env variables
 const client = new Client({
-  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN || "",
-  channelSecret: process.env.CHANNEL_SECRET || "",
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN || '',
+  channelSecret: process.env.CHANNEL_SECRET || '',
 });
 const { CONCAT_ID } = process.env;
 function handleEvent(event: any) {
-  if (event.source.userId === "Udeadbeefdeadbeefdeadbeefdeadbeef") return; // webhook verify
+  if (event.source.userId === 'Udeadbeefdeadbeefdeadbeefdeadbeef') return; // webhook verify
 
+  let templateId: number = 1,
+    templateLink: string = `https://liff.line.me/${CONCAT_ID}/?template=${templateId}`;
   const flexQuery: flexUrlTemplate = {
-    title: "I am title",
-    place: "@10-4",
-    time: "10:30~11:00",
-    url: `https://liff.line.me/${CONCAT_ID}/?template=1`,
-    imageUrl: "https://i.imgur.com/EI8AuUY.jpg",
-    description: "description",
-    activity: "活動",
+    title: 'I am title',
+    place: '@10-4',
+    time: '10:30~11:00',
+    url: templateLink,
+    imageUrl: 'https://i.imgur.com/EI8AuUY.jpg',
+    description: 'description',
+    activity: '活動',
   };
+  templateId = 2;
   const staffQuery: staffList = {
-    title: "10/10 Title",
-    place: "台北市內湖區瑞光路",
-    map: `https://liff.line.me/${CONCAT_ID}/?template=2`,
-    url: `https://liff.line.me/${CONCAT_ID}/?template=2`,
-    activity: "參與名單",
+    title: '10/10 Title',
+    place: '台北市內湖區瑞光路',
+    map: templateLink,
+    url: templateLink,
+    activity: '參與名單',
     people: [
-      { name: "Moon", time: "10:00~12:00" },
-      { name: "Boss", time: "11:00~12:00" },
+      { name: 'Moon', time: '10:00~12:00' },
+      { name: 'Boss', time: '11:00~12:00' },
     ],
   };
+  templateId = 3;
   const person: Card = {
-    title: "NiJia(testing)",
-    description: "Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem ",
-    url: `https://liff.line.me/${CONCAT_ID}/?template=3`,
+    title: 'NiJia(testing)',
+    description: 'Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem ',
+    followUrl: templateLink,
+    back: templateLink,
+    avatar: templateLink,
   };
   const flex: FlexMessage = buildFlexContent(
-    "測試數據",
+    '測試數據',
     buildCarouselContent([
       generateFlex(flexQuery),
       activitySchedule(staffQuery),
       personalCard(person),
     ])
   );
+
   console.log(`Reply message: ${JSON.stringify(personalCard(person))}`);
   return client.replyMessage(event.replyToken, flex);
 }
